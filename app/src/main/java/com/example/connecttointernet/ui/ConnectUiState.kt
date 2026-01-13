3b52c8ecbde235ecef8f21bb82ac6be0c1a9fc5b
Carlos Bermúdez Mancebo <carlosbermudezmancebo@gmail.com>
2026-01-13 18:58:34 +0100

    ConnectUiState.kt -> image link added

--- app/src/main/java/com/example/connecttointernet/ui/ConnectUiState.kt
+++ app/src/main/java/com/example/connecttointernet/ui/ConnectUiState.kt
@@ -1,5 +1,5 @@
 package com.example.connecttointernet.ui
 
 data class ConnectUiState (
-
+    val imagen : String = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png"
 )
