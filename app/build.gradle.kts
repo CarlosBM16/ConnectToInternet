d2c9136fedefceeebda54326a26e336e70f5f1a9
GITCarlosBarrera <carlos.barrera.2003@gmail.com>
2026-01-13 19:13:03 +0100

    Implementacion en el gradle y comienzo de el ConnectScreen.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ android {
 
 dependencies {
 
+    implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/example/connecttointernet/ui/ConnectScreen.kt
+++ app/src/main/java/com/example/connecttointernet/ui/ConnectScreen.kt
@@ -1,4 +1,35 @@
 package com.example.connecttointernet.ui
 
-class ConnectScreen {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
+
+@Composable
+fun ConnectScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            AsyncImage(
+                model = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png",
+                contentDescription = null
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewConnectScreen() {
+    ConnectScreen()
 }
