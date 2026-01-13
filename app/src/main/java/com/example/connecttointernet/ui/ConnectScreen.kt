8ba22d00cfdb7746fb77938254df90de95042060
GITCarlosBarrera <carlos.barrera.2003@gmail.com>
2026-01-13 20:12:38 +0100

    ConnectScreen.kt -> Very really important change Part 2

--- app/src/main/java/com/example/connecttointernet/ui/ConnectScreen.kt
+++ app/src/main/java/com/example/connecttointernet/ui/ConnectScreen.kt
@@ -5,13 +5,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 
@@ -29,6 +32,11 @@ fun ConnectScreen(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Text(
+                text = "\uD83C\uDFB5POKEMON HAZTE CON TOOODOOOS\uD83C\uDFB5",
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp
+            )
             AsyncImage(
                 modifier = Modifier.fillMaxSize(),
                 model = connectUiState.image.url,
